a896d287aff37fe227dc3477ba47cbde1a0f3de9
Christian Anderson <christian.p.anderson1@gmail.com>
2021-02-03 22:25:30 -0500

    added test for lombok

--- src/test/java/com/reddit/springredditclone/UserIntegrationTest.java
+++ src/test/java/com/reddit/springredditclone/UserIntegrationTest.java
@@ -1,2 +1,22 @@
-package com.reddit.springredditclone;public class UserIntegrationTest {
+package com.reddit.springredditclone;
+
+import lombok.Setter;
+import lombok.Getter;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserIntegrationTest {
+
+    @Test
+    public void givenAnnotatedUser_thenHasGettersAndSetters() {
+        User user = new User();
+        user.setFirstName("Test");
+        assertEquals(user.getFirstName(), "Test");
+    }
+
+    @Getter @Setter
+    class User {
+        private String firstName;
+    }
 }
