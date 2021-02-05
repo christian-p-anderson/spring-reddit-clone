460f14a9a8bae3884c73c1972e753ef556a044f2
Christian Anderson <christian.p.anderson1@gmail.com>
2021-02-04 23:14:25 -0500

    created Post Entity

--- src/main/java/com/reddit/springredditclone/model/Post.java
+++ src/main/java/com/reddit/springredditclone/model/Post.java
@@ -1,4 +1,39 @@
 package com.reddit.springredditclone.model;
 
+import com.sun.istack.Nullable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+import java.time.Instant;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Post {
+    @Id
+    @GeneratedValue(strategy = SEQUENCE)
+    private Long postId;
+    @NotBlank(message = "Post Name cannot be empty or Null")
+    private String postName;
+    @Nullable
+    @Lob
+    private String description;
+    private Integer voteCount;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+    private Instant createdDate;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    private Subreddit subreddit;
 }
