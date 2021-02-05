eff67dc5f0add2cc9e462842b538b578bc13df3f
Christian Anderson <christian.p.anderson1@gmail.com>
2021-02-04 23:09:23 -0500

    created User Entity

--- src/main/java/com/reddit/springredditclone/model/User.java
+++ src/main/java/com/reddit/springredditclone/model/User.java
@@ -1,4 +1,33 @@
 package com.reddit.springredditclone.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+import java.time.Instant;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class User {
+        @Id
+        @GeneratedValue(strategy = SEQUENCE)
+        private Long userId;
+        @NotBlank(message = "Username is required")
+        private String password;
+        @Email
+        @NotEmpty(message = "Email is required")
+        private String email;
+        private Instant created;
+        private boolean enabled;
 }
