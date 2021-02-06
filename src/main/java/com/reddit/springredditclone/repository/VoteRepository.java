59ffa98d82140f7c95a3825d8c5f998e6bf9decd
Christian Anderson <christian.p.anderson1@gmail.com>
2021-02-06 00:32:57 -0500

    created VoteRepository file

--- src/main/java/com/reddit/springredditclone/repository/VoteRepository.java
+++ src/main/java/com/reddit/springredditclone/repository/VoteRepository.java
@@ -1,4 +1,12 @@
 package com.reddit.springredditclone.repository;
 
-public interface VoteRepository {
+import com.reddit.springredditclone.model.Post;
+import com.reddit.springredditclone.model.User;
+import com.reddit.springredditclone.model.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+    Optional<Vote> findTopByPostAndUserOrderByVoteIdDesc(Post post, User currentUser);
 }
