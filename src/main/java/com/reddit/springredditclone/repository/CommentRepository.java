0bda57612f9733a4d657ccfde285c5a362ba95ef
Christian Anderson <christian.p.anderson1@gmail.com>
2021-02-06 00:29:31 -0500

    created CommentRepository file

--- src/main/java/com/reddit/springredditclone/repository/CommentRepository.java
+++ src/main/java/com/reddit/springredditclone/repository/CommentRepository.java
@@ -1,4 +1,15 @@
 package com.reddit.springredditclone.repository;
 
-public interface CommentRepository {
+import com.reddit.springredditclone.model.Comment;
+import com.reddit.springredditclone.model.Post;
+import com.reddit.springredditclone.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPost(Post post);
+
+    List<Comment> findAllByUser(User user);
+
 }
