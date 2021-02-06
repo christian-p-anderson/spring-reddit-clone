140a2e46dc6d7aa231ec486716d59ecb6a4e92d1
Christian Anderson <christian.p.anderson1@gmail.com>
2021-02-06 00:27:33 -0500

    created PostRepository file

--- src/main/java/com/reddit/springredditclone/repository/PostRepository.java
+++ src/main/java/com/reddit/springredditclone/repository/PostRepository.java
@@ -1,4 +1,14 @@
 package com.reddit.springredditclone.repository;
 
-public interface PostRepository {
+import com.reddit.springredditclone.model.Post;
+import com.reddit.springredditclone.model.Subreddit;
+import com.reddit.springredditclone.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllBySubreddit(Subreddit subreddit);
+
+    List<Post> findByUser(User user);
 }
