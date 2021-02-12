6be5d4057c783d755f59cad9fd7d3cc7f778b3e4
Christian Anderson <christian.p.anderson1@gmail.com>
2021-02-11 20:17:46 -0500

    modified AuthService to include email verification

--- src/main/java/com/reddit/springredditclone/service/AuthService.java
+++ src/main/java/com/reddit/springredditclone/service/AuthService.java
@@ -1,6 +1,8 @@
 package com.reddit.springredditclone.service;
 
 import com.reddit.springredditclone.dto.RegisterRequest;
+import com.reddit.springredditclone.exception.SpringRedditException;
+import com.reddit.springredditclone.model.NotificationEmail;
 import com.reddit.springredditclone.model.User;
 import com.reddit.springredditclone.model.VerificationToken;
 import com.reddit.springredditclone.repository.UserRepository;
@@ -11,9 +13,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-
 import java.util.UUID;
 
+import static com.reddit.springredditclone.util.Constants.ACTIVATION_EMAIL;
 import static java.time.Instant.now;
 
 @Service
@@ -24,6 +26,8 @@ public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final VerificationTokenRepository verificationTokenRepository;
+    private final MailContentBuilder mailContentBuilder;
+    private final MailService mailService;
 
     @Transactional
     public void signup (RegisterRequest registerRequest) {
@@ -37,6 +41,10 @@ public class AuthService {
         userRepository.save(user);
 
         String token = generateVerificationToken(user);
+        String message =  mailContentBuilder.build("Thank you for signing up to Spring Reddit, please click on the below url to activate your account : "
+        + ACTIVATION_EMAIL + "/" + token);
+
+        mailService.sendMail(new NotificationEmail("Please Activate your account", user.getEmail(), message));
 
     }
 
@@ -52,5 +60,13 @@ public class AuthService {
 
     private String encodePassword(String password) {
         return passwordEncoder.encode(password);
+    }
+
+    @Transactional
+    public void fetchUserAndEnable(VerificationToken verificationToken) {
+        String username = verificationToken.getUser().getUsername();
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new SpringRedditException("User Not Found with ID - " + username));
+        user.setEnabled(true);
+        userRepository.save(user);
     }
 }
