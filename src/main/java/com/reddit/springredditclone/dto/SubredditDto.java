1abc9e1d8b4c9fb33105ca6adc4efce7a99ae5ba
Christian Anderson <christian.p.anderson1@gmail.com>
2021-02-18 20:41:25 -0500

    created Subreddit Dto

--- src/main/java/com/reddit/springredditclone/dto/SubredditDto.java
+++ src/main/java/com/reddit/springredditclone/dto/SubredditDto.java
@@ -1,2 +1,28 @@
-package com.reddit.springredditclone.dto;public class SubredditDto {
+package com.reddit.springredditclone.dto;
+
+import javassist.bytecode.MethodParametersAttribute;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubredditDto {
+    private Long id;
+    private String name;
+    private String description;
+    private Integer postCount;
+
+
+//    public static MethodParametersAttribute builder() {
+//    }
+//
+//    public String getDescription() {
+//    }
+//
+//    public String getName() {
+//    }
 }
