cbc4066df3466c641372fa7386953d20dae1b860
Christian Anderson <christian.p.anderson1@gmail.com>
2021-02-18 20:22:29 -0500

    setup controller to create and read subreddits

---
+++ src/main/java/com/reddit/springredditclone/controller/SubRedditController.java
@@ -0,0 +1,31 @@
+package com.reddit.springredditclone.controller;
+
+import com.reddit.springredditclone.dto.SubredditDto;
+import com.reddit.springredditclone.service.SubredditService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/subreddit")
+@AllArgsConstructor
+public class SubRedditController {
+    private final SubredditService subredditService;
+
+    @GetMapping
+    public List<SubredditDto> getAllSubreddits() {
+        return subredditService.getAll();
+    }
+
+    @GetMapping("/{id")
+    public SubredditDto getSubreddit(@PathVariable Long id) {
+        return subredditService.getSubreddit(id);
+    }
+
+    @PostMapping
+    public SubredditDto create(@RequestBody @Valid SubredditDto subredditDto) {
+        return subredditService.save(subredditDto);
+    }
+}
---
+++ src/main/java/com/reddit/springredditclone/service/SubredditService.java
@@ -0,0 +1,2 @@
+package com.reddit.springredditclone.service;public class SubredditService {
+}
